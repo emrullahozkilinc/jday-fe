1bb515b696c399fdd8cf8bda3665486b08fbac46
emrullahozkilinc <emr.ozk.71@gmail.com>
2023-07-09 21:53:53 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import Stepper from "./components/step/Stepper";
+import StepComponent from "./components/step/StepComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+        <div class="row">
+            <div class="col-md-12">
+
+                <ul class="stepper stepper-horizontal">
+
+                    <li class="completed">
+                        <a href="#!">
+                            <span class="circle">1</span>
+                            <span class="label">First step</span>
+                        </a>
+                    </li>
+
+                    <li class="active">
+                        <a href="#!">
+                            <span class="circle">2</span>
+                            <span class="label">Second step</span>
+                        </a>
+                    </li>
+
+                    <li class="warning">
+                        <a href="#!">
+                            <span class="circle"><i class="fas fa-exclamation"></i></span>
+                            <span class="label">Third step</span>
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
     </div>
   );
 }
